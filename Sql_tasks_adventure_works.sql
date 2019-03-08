503522e4a5d89f44bc74ce2a1b497c558e977040
karolinatestova <karolina.testova@gmail.com>
2019-03-08 17:54:20 +0100

    6. Show Product number and List price of Product of name Paint - Yellow.

--- Sql_tasks_adventure_works.sql
+++ Sql_tasks_adventure_works.sql
@@ -124,3 +124,29 @@ WHERE
 		c.CustomerID = 29520
 
 --------------------------------------------------------------------------------------------
+
+--6. Show Product number and List price of Product of name Paint - Yellow  
+
+USE AdventureWorks
+GO
+
+----
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'ProductNumber'
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME like '%price%'
+
+----
+
+SELECT 
+	ProductNumber, ListPrice, Name
+FROM 
+	[Production].[Product]
+WHERE 
+	Name = 'Paint - Yellow '
+
+--------------------------------------------------------------------------------------------
