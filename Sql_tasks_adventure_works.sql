b45c6372801650e96aaf9ed2b3f662a54ead3b35
karolinatestova <karolina.testova@gmail.com>
2019-03-08 17:49:46 +0100

    5. Show OrderQty, the Name and the ListPrice of the order made by CustomerID 29520.

--- Sql_tasks_adventure_works.sql
+++ Sql_tasks_adventure_works.sql
@@ -82,3 +82,45 @@ FROM
 
 --------------------------------------------------------------------------------------------
 
+--5. Show OrderQty, the Name and the ListPrice of the order made by CustomerID 29520.
+
+USE AdventureWorks
+GO
+
+----
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'OrderQty'
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'name'
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'listPrice'
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'CustomerID'
+
+----
+
+SELECT  [Name], 
+		[ListPrice], 
+		[OrderQty],
+		c.[CustomerID]
+
+FROM			[Production].[Product] p
+	INNER JOIN  [Sales].[SpecialOfferProduct] sop
+			ON p.[ProductID] = sop.[ProductID]
+	INNER JOIN [Sales].[SalesOrderDetail] sod
+			ON sop.[SpecialOfferID] = sod.[SpecialOfferID]
+	INNER JOIN [Sales].[SalesOrderHeader] soh
+			ON sod.[SalesOrderID] = soh.[SalesOrderID]
+	INNER JOIN [Sales].[Customer] c
+			ON soh.[CustomerID] = c.[CustomerID]
+WHERE
+		c.CustomerID = 29520
+
+--------------------------------------------------------------------------------------------
